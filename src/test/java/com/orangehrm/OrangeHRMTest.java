432d63809a9d3ef51a2bb01d6bb15757cf7dcf4d
Michal <s24407@pjwstk.edu.pl>
2025-02-03 16:29:07 +0100

    optimize imports

--- src/test/java/com/orangehrm/OrangeHRMTest.java
+++ src/test/java/com/orangehrm/OrangeHRMTest.java
@@ -4,7 +4,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
