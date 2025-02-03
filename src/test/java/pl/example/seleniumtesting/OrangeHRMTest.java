4d1e20adbdfa9dbfd3c1847e6a7a45f8b1f0abe0
Michal <s24407@pjwstk.edu.pl>
2025-02-03 14:47:54 +0100

    OrangeHRM login test

---
+++ src/test/java/pl/example/seleniumtesting/OrangeHRMTest.java
@@ -0,0 +1,62 @@
+package pl.example.seleniumtesting;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.visibilityOfElementLocated;
+
+public class OrangeHRMTest {
+    WebDriver driver;
+    WebDriverWait wait;
+
+    @BeforeClass
+    public void setUp() {
+        driver = new ChromeDriver();
+        driver.manage()
+                .window()
+                .maximize();
+        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+
+    @AfterClass
+    public void tearDown() {
+        driver.quit();
+    }
+
+    @Test
+    public void testLoggingInApplication() throws InterruptedException {
+
+        var username = wait.until(
+                visibilityOfElementLocated(
+                        By.name("username")
+                )
+        );
+
+        username.sendKeys("Admin");
+
+        var password = driver.findElement(By.name("password"));
+        password.sendKeys("admin123");
+
+        driver.findElement(By.tagName("button")).click();
+
+        var header = wait.until(
+                visibilityOfElementLocated(
+                        By.tagName("h6")
+                )
+        );
+
+        String result = header.getText();
+        String expectedResult = "Dashboard";
+
+        Assert.assertEquals(result, expectedResult);
+    }
+}
